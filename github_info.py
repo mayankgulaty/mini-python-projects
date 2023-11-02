30bc7375b41c2d8abfdd07a993fe0180ef3f8695
Mayank Gulaty <mayankgulaty@gmail.com>
2023-11-02 08:49:08 +0000

    Bit more improvements

--- github_info.py
+++ github_info.py
@@ -1,7 +1,7 @@
 import requests
+import os
 
-
-def get_github_stats(username):
+def get_github_stats(username, sort_by="stargazers_count", reverse=False):
     # Set your GitHub personal access token as an environment variable
     github_token = "Your Github Token"
     if github_token is None:
@@ -20,11 +20,19 @@ def get_github_stats(username):
         response.raise_for_status()
         repos = response.json()
 
+        # Sort the repositories based on the selected criteria
+        repos.sort(key=lambda repo: repo[sort_by], reverse=reverse)
+
         print(f"GitHub Stats for {username}:")
         print(f"Total Repositories: {len(repos)}")
         print("Repository List:")
         for repo in repos:
             print(f"- {repo['name']}")
+            print(f"  - Stars: {repo['stargazers_count']}")
+            print(f"  - Forks: {repo['forks_count']}")
+            print(f"  - Watchers: {repo['watchers_count']}")
+            print(f"  - Last Updated: {repo['updated_at']}")
+            print(f"  - Description: {repo['description']}\n")
 
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data from GitHub: {e}")
@@ -32,4 +40,11 @@ def get_github_stats(username):
 
 if __name__ == "__main__":
     github_username = input("Enter your GitHub username: ")
-    get_github_stats(github_username)
+    sort_criteria = input("Sort by (stargazers_count, forks, watchers, updated): ")
+    reverse_sort = input("Reverse sort? (True/False): ").lower() == "true"
+
+    if sort_criteria not in ["stargazers_count", "forks", "watchers", "updated_at"]:
+        print("Invalid sorting criteria. Using default: stargazers_count")
+        sort_criteria = "stargazers_count"
+
+    get_github_stats(github_username, sort_criteria, reverse_sort)
