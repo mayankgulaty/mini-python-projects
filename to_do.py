4bb4526752554ec42008bb2d0eef4fac7162aa09
Mayank Gulaty <mayankgulaty@gmail.com>
2023-11-04 11:34:00 +0000

    Added the GUI Part

--- to_do.py
+++ to_do.py
@@ -1,62 +1,39 @@
-import json
+import tkinter as tk
+from tkinter import messagebox
 
-# Load existing tasks from a JSON file
-def load_tasks():
+def add_task():
+    task = entry.get()
+    if task:
+        listbox.insert(tk.END, task)
+        entry.delete(0, tk.END)
+    else:
+        messagebox.showwarning("Warning", "Please enter a task.")
+
+def delete_task():
     try:
-        with open("tasks.json", "r") as file:
-            return json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        return []
+        selected_task_index = listbox.curselection()[0]
+        listbox.delete(selected_task_index)
+    except IndexError:
+        pass
 
-# Save tasks to a JSON file
-def save_tasks(tasks):
-    with open("tasks.json", "w") as file:
-        json.dump(tasks, file, indent=4)
+root = tk.Tk()
+root.title("To-Do List")
+root.geometry("400x400")
+root.configure(bg="#EFEFEF")
 
-# Add a new task
-def add_task(task):
-    tasks = load_tasks()
-    tasks.append({"task": task, "done": False})
-    save_tasks(tasks)
-    print("Task added successfully!")
+entry = tk.Entry(root, width=30, font=("Helvetica", 14))
+entry.pack(pady=20)
 
-# List all tasks
-def list_tasks():
-    tasks = load_tasks()
-    if not tasks:
-        print("No tasks found.")
-    else:
-        print("Tasks:")
-        for idx, task in enumerate(tasks, start=1):
-            status = "Done" if task["done"] else "Not Done"
-            print(f"{idx}. {task['task']} - {status}")
+button_frame = tk.Frame(root, bg="#EFEFEF")
+button_frame.pack()
 
-# Mark a task as done
-def mark_done(task_index):
-    tasks = load_tasks()
-    if 1 <= task_index <= len(tasks):
-        tasks[task_index - 1]["done"] = True
-        save_tasks(tasks)
-        print("Task marked as done!")
-    else:
-        print("Invalid task index.")
+# Set the text color to black
+add_button = tk.Button(button_frame, text="Add Task", command=add_task, font=("Helvetica", 12), bg="#008CBA", fg="black")
+delete_button = tk.Button(button_frame, text="Delete Task", command=delete_task, font=("Helvetica", 12), bg="#FF5733", fg="black")
+add_button.grid(row=0, column=0, padx=10)
+delete_button.grid(row=0, column=1)
 
-# Main program loop
-while True:
-    print("To-Do List Menu:")
-    print("1. Add Task")
-    print("2. List Tasks")
-    print("3. Mark Task as Done")
-    print("4. Quit")
-    choice = input("Enter your choice (1/2/3/4): ")
+listbox = tk.Listbox(root, selectmode=tk.SINGLE, font=("Helvetica", 12), bg="#F0F0F0")
+listbox.pack(pady=20, fill=tk.BOTH, expand=True)
 
-    if choice == "1":
-        task = input("Enter the task: ")
-        add_task(task)
-    elif choice == "2":
-        list_tasks()
-    elif choice == "3":
-        task_index = int(input("Enter the task number to mark as done: "))
-        mark_done(task_index)
-    elif choice == "4":
-        break
+root.mainloop()
