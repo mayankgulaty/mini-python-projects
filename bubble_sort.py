1bb90cca1c6bdd341c188ffb6ae969f4a7f2a59f
Mayank Gulaty <mayankgulaty@gmail.com>
2023-11-14 10:20:37 +0000

    put numbers to make it more understandable

--- bubble_sort.py
+++ bubble_sort.py
@@ -9,17 +9,24 @@ pygame.init()
 WIDTH, HEIGHT = 800, 600
 BACKGROUND_COLOR = (255, 255, 255)
 BAR_COLOR = (0, 0, 255)
+TEXT_COLOR = (255, 255, 255)
 SWAP_COLOR = (255, 0, 0)
-FPS = 60
+FPS = 10  # Reduced the frames per second for smoother animation
 
-# Function to draw bars
+# Function to draw bars with numbers
 def draw_bars(screen, bars, highlight=None):
     screen.fill(BACKGROUND_COLOR)
+    font = pygame.font.Font(None, 36)
+
     for i, height in enumerate(bars):
         color = BAR_COLOR
         if highlight and (i == highlight[0] or i == highlight[1]):
             color = SWAP_COLOR
         pygame.draw.rect(screen, color, (i * (WIDTH // len(bars)), HEIGHT - height, WIDTH // len(bars), height))
+
+        text = font.render(str(height), True, TEXT_COLOR)
+        text_rect = text.get_rect(center=(i * (WIDTH // len(bars)) + (WIDTH // len(bars)) // 2, HEIGHT - height // 2))
+        screen.blit(text, text_rect)
 
 # Bubble Sort Algorithm
 def bubble_sort(bars, screen):
@@ -29,7 +36,7 @@ def bubble_sort(bars, screen):
             # Draw bars with highlight for comparison
             draw_bars(screen, bars, highlight=(j, j+1))
             pygame.display.flip()
-            pygame.time.delay(50)  # Delay for visualization
+            pygame.time.delay(500)  # Delay for visualization
 
             if bars[j] > bars[j+1]:
                 # Swap bars
@@ -37,12 +44,12 @@ def bubble_sort(bars, screen):
                 # Draw bars with highlight for swap
                 draw_bars(screen, bars, highlight=(j, j+1))
                 pygame.display.flip()
-                pygame.time.delay(50)  # Delay for visualization
+                pygame.time.delay(500)  # Delay for visualization
 
 # Main function
 def main():
     # Generate random bar heights
-    bars = [random.randint(50, 500) for _ in range(50)]
+    bars = [random.randint(50, 500) for _ in range(10)]
 
     # Create Pygame window
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
