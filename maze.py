eebb93be1b3a99c1db41eff011ffc027d3e74064
Mayank Gulaty <mayankgulaty@gmail.com>
2023-11-28 21:21:14 +0000

    a bit better visual

--- maze.py
+++ maze.py
@@ -1,9 +1,9 @@
 import random
 
 def generate_maze(size):
-    maze = [["#" if random.random() < 0.3 else " " for _ in range(size)] for _ in range(size)]
-    maze[0][0] = "S"  # Start
-    maze[-1][-1] = "E"  # End
+    maze = [["🌿" if random.random() < 0.3 else " " for _ in range(size)] for _ in range(size)]
+    maze[0][0] = "🚶"  # Player
+    maze[-1][-1] = "🏁"  # Finish
     return maze
 
 def display_maze(maze):
@@ -11,8 +11,6 @@ def display_maze(maze):
         print(" ".join(row))
 
 def navigate_maze(maze):
-    current_position = [0, 0]
-
     while True:
         display_maze(maze)
 
@@ -22,37 +20,47 @@ def navigate_maze(maze):
             print("Quitting the maze.")
             break
         elif user_move in ("W", "A", "S", "D"):
-            move_result = make_move(maze, current_position, user_move)
+            move_result = make_move(maze, user_move)
             if move_result == "E":
                 print("Congratulations! You reached the end of the maze.")
                 break
         else:
             print("Invalid move. Use W/A/S/D to move or Q to quit.")
 
-def make_move(maze, current_position, direction):
-    x, y = current_position
+def make_move(maze, direction):
+    player_position = find_player(maze)
+    x, y = player_position
 
-    if direction == "W" and x > 0 and maze[x - 1][y] != "#":
+    if direction == "W" and x > 0 and maze[x - 1][y] != "🌿":
         maze[x][y] = " "
         x -= 1
-    elif direction == "A" and y > 0 and maze[x][y - 1] != "#":
+    elif direction == "A" and y > 0 and maze[x][y - 1] != "🌿":
         maze[x][y] = " "
         y -= 1
-    elif direction == "S" and x < len(maze) - 1 and maze[x + 1][y] != "#":
+    elif direction == "S" and x < len(maze) - 1 and maze[x + 1][y] != "🌿":
         maze[x][y] = " "
         x += 1
-    elif direction == "D" and y < len(maze[0]) - 1 and maze[x][y + 1] != "#":
+    elif direction == "D" and y < len(maze[0]) - 1 and maze[x][y + 1] != "🌿":
         maze[x][y] = " "
         y += 1
 
-    current_position[0], current_position[1] = x, y
+    maze[x][y] = "🚶"
 
-    if maze[x][y] == "E":
+    if maze[x][y] == "🏁":
         return "E"
+    else:
+        return None
+
+
+def find_player(maze):
+    for i, row in enumerate(maze):
+        for j, cell in enumerate(row):
+            if cell == "🚶":
+                return i, j
 
 if __name__ == "__main__":
     maze_size = int(input("Enter the size of the maze: "))
     maze = generate_maze(maze_size)
 
-    print("Welcome to the Maze Game!")
+    print("Welcome to the Emoji Maze Game! 🌟")
     navigate_maze(maze)
