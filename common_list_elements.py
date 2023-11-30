efb94c9a085bf27a71525c6208ce627e6e9326de
Mayank Gulaty <mayankgulaty@gmail.com>
2023-11-30 22:22:09 +0000

    added  comments

--- common_list_elements.py
+++ common_list_elements.py
@@ -1,13 +1,25 @@
 def find_common_elements(list1, list2):
-    common_elements = set(list1) & set(list2)
+    # Convert the input lists to sets for efficient intersection operation
+    set1 = set(list1)
+    set2 = set(list2)
+
+    # Find the common elements using the intersection of sets
+    common_elements = set1 & set2
+
+    # Convert the result set back to a list
     return list(common_elements)
 
 if __name__ == "__main__":
+    # Input elements for the first list
     list1 = input("Enter elements for the first list (comma-separated): ").split(',')
+
+    # Input elements for the second list
     list2 = input("Enter elements for the second list (comma-separated): ").split(',')
 
+    # Call the utility function to find common elements
     common_elements = find_common_elements(list1, list2)
 
+    # Display the result
     if common_elements:
         print(f"Common elements: {common_elements}")
     else:
